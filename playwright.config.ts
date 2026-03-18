40ced026abc74ebd403393bd4c3c993e86b09ec1
Vinícius Varjão <moraesvxp@gmail.com>
2026-03-18 15:21:48 -0300

    test(e2e): habilita webServer automático no playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,4 +21,10 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
   ],
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:4200",
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
